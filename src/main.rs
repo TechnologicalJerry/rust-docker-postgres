54504e2a129897ac5c0794045a4875f38239d55c
technologicaljerry <cserajatverma@gmail.com>
2023-05-10 23:38:38 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, Welcome to Rust Application!");
-}
+use postgres::Error as PostgresError;
+use postgres::{Client, NoTls};
+use std::env;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
