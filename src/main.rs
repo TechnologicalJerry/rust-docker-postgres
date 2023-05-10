339830b2338ec056c0ca79d55ccf495641c54485
technologicaljerry <cserajatverma@gmail.com>
2023-05-10 23:39:25 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,10 @@ use std::net::{TcpListener, TcpStream};
 
 #[macro_use]
 extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
