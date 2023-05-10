80f633b8ac48c3af994e45ffa28995045e448fce
technologicaljerry <cserajatverma@gmail.com>
2023-05-10 23:39:55 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,7 @@ struct User {
 }
 
 const DB_URL: &str = env!("DATABASE_URL");
+
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
