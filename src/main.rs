dfbdc53d75c83f698c5a471be362e6739e302d67
technologicaljerry <cserajatverma@gmail.com>
2023-05-10 23:40:34 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,24 @@ const DB_URL: &str = env!("DATABASE_URL");
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+
+fn main() {
+    if let Err(e) = set_database() {
+        println!("Error: {}", e);
+        return;
+    }
+
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server started at port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
