a9a3c60408b0c0885373499e0ed0f7bee8bdb6fd
technologicaljerry <cserajatverma@gmail.com>
2023-05-11 22:06:20 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -157,9 +157,14 @@ fn handle_put_request(request: &str) -> (String, String) {
 }
 
 fn handle_delete_request(request: &str) -> (String, String) {
-    match (get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+    match (
+        get_id(&request).parse::<i32>(),
+        Client::connect(DB_URL, NoTls),
+    ) {
         (Ok(id), Ok(mut client)) => {
-            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
+            let rows_affected = client
+                .execute("DELETE FROM users WHERE id = $1", &[&id])
+                .unwrap();
 
             if rows_affected == 0 {
                 return (NOT_FOUND.to_string(), "User not found".to_string());
